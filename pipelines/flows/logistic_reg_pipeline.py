b92459acdca305d7023c95638517221b5e8f984f
Elaine Chen <xtchen@Elaines-MacBook-Pro.local>
2023-10-28 20:43:52 -0700

    Enabled writing results to csv.

--- pipelines/flows/logistic_reg_pipeline.py
+++ pipelines/flows/logistic_reg_pipeline.py
@@ -1,9 +1,10 @@
 import gin
 from models.logistic_reg import LogisticReg
 from data_processors.base_data_processor import BaseDataProcessor
+from utils.common_utils import write_results_to_file
 
 @gin.configurable
-def main(configs):
+def main(configs, write_results=True):
     # TODO: add more configs as gin configurable (e.g. computation budget)
     # TODO: configure GPU for ray tune (e.g. might need to use specific packages)
 
@@ -35,6 +36,14 @@ def main(configs):
     acc, f1 = lr.evaluate(lr.model, test_X, test_y)
     print("Test acc: {}".format(acc))
     print("Test f1: {}".format(f1))
+
+    # write result to csv
+    if write_results:
+        write_results_to_file(
+            acc, 
+            f1, 
+            model_name="logistic_reg", 
+            filename="/Users/xtchen/Projects/deep-learning-with-mnist/result.csv")
 
 if __name__ == "__main__":
     gin.parse_config_file('pipelines/configs/logistic_reg_pipeline.gin')
--- utils/common_utils.py
+++ utils/common_utils.py
@@ -17,3 +17,46 @@ def auto_assign(func):
         func(self, *args, **kwargs)
         
     return wrapper
+
+def write_results_to_file(acc, f1, model_name, filename="result.csv"):
+    """
+    Writes the results to the given file.
+    :param acc: accuracy
+    :param f1: f1 score
+    :param model_name: name of the model
+    :param filename: name of the file to write to
+    """
+    # Initialize header and data lines
+    header = "model,acc,f1\n"
+    data_line = f"{model_name},{acc},{f1}\n"
+
+    # Check if file exists
+    try:
+        with open(filename, 'r') as file:
+            content = file.readlines()
+
+            # Check if header exists
+            if content:
+                if header.strip() not in content[0]:
+                    content.insert(0, header)
+            else:
+                content.append(header)
+
+            # Check if model line exists
+            model_exists = False
+            for line in content:
+                if line.startswith(model_name):
+                    model_exists = True
+                    break
+            
+            if not model_exists:
+                content.append(data_line)
+
+            new_content = ''.join(content)
+
+    except FileNotFoundError:
+        new_content = header + data_line
+
+    # Write the updated content back to the file
+    with open(filename, 'w') as file:
+        file.write(new_content)
